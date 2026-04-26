8a29a912d79959d40a8737101e03adbf895b985f
Maida Kucevic <145588313+maida5@users.noreply.github.com>
2026-04-26 00:27:13 +0000

    map iframe

--- hunter-hacks-app/src/App.js
+++ hunter-hacks-app/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 
 function App() {
   return (
-      <div>
-        <h1>
-          Hunter Hacks!
-        </h1>
+      <div className="App">
+        <h1>Hunter Hacks!</h1>
+        <iframe
+          title="map"
+          src="/fare_evasion_map.html"
+          style={{ width: '100%', height: '80vh', border: 'none' }}
+        />
       </div>
     
   );
