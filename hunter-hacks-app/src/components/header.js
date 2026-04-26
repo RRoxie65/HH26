7702777613d458f1f2992c0a4507dcedae854bbb
maida5 <145588313+maida5@users.noreply.github.com>
2026-04-26 11:03:02 -0400

    Revert "change name"
    This reverts commit a45674c8920a99b7ebc00446b688000c144ffcc4.

--- hunter-hacks-app/src/components/header.js
+++ hunter-hacks-app/src/components/header.js
@@ -3,7 +3,7 @@ import "./header.css";
 function Header() {
    return (
       <header className='heading'>
-         <span>FareCity?</span>
+         <span>Name</span>
          <nav>
             <a href='#map'>Map</a>
             <a href='#about'>About</a>
