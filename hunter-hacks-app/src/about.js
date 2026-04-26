39afcd67b1a8227eb0fd932ada67d521fd0ac3f0
Bonnie Lei <bonnielei91@gmail.com>
2026-04-26 11:11:02 -0400

    update repo

--- hunter-hacks-app/src/about.js
+++ hunter-hacks-app/src/about.js
@@ -5,7 +5,8 @@ function About() {
       <>
          <h1>About this project</h1>
          <p>
-            This project was created as part of the HunterHacks26 at Hunter College. The goal of this project is to analyze fare evasion in the New York City subway system and provide insights into which stations have the highest rates of fare evasion.</p>
+            This project was created as part of the HunterHacks26 at Hunter College. The goal of this project is to analyze fare evasion in the New York City subway system and provide insights into which stations have the highest rates of fare evasion.
+         </p>
          <p>
             We used data from the MTA's Subway Stations Evasion Percentage dataset, which provides information on the percentage of fare evasion at each subway station. We showcase a map of the New York City subway system with the fare evasion data overlaid. We also used Python and various libraries such as Pandas for convenient data analysis.
          </p>
