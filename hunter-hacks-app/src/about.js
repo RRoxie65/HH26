ba1bd2c01c711ac17441cf3007f4682a10e91066
Bonnie Lei <bonnielei91@gmail.com>
2026-04-26 11:20:32 -0400

    update repo

--- hunter-hacks-app/src/about.js
+++ hunter-hacks-app/src/about.js
@@ -11,7 +11,7 @@ function About() {
             We used data from the MTA's Subway Stations Evasion Percentage dataset, which provides information on the percentage of fare evasion at each subway station. We showcase a map of the New York City subway system with the fare evasion data overlaid. We also used Python and various libraries such as Pandas for convenient data analysis.
          </p>
          <p>
-            Our findings show that certain stations, particularly those in lower-income neighborhoods, have higher rates of fare evasion. This highlights the need for targeted interventions to address fare evasion and ensure that all riders are paying their fair share.
+            Our findings show that certain stations have higher rates of fare evasion. This highlights the need for targeted interventions to address fare evasion and ensure that all riders are paying their fair share. Additionally, to stations with missing data, we hope to encourage the MTA to collect and publish more comprehensive data for better analysis.
          </p>
       </>
    );
